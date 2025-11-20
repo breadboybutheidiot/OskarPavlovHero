15aac0f608f77b279353238d9b5b100297b10640
pavlovaisip23_3 <psjrix00@gmail.com>
2025-11-20 15:02:26 +0900

    yooo cheskisout

--- app/src/main/java/com/example/oskarpavlovhero/ui/theme/Theme.kt
+++ app/src/main/java/com/example/oskarpavlovhero/ui/theme/Theme.kt
@@ -83,7 +83,7 @@ private val DarkColors = darkColorScheme(
 )
 
 @Composable
-fun WoofTheme(
+fun OskarPavlovHeroTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = false,
